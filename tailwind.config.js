f3f023d27e3ab68346b9ddd79fb2d7ad7db4a3af
rio1210-fe <rio.rubiarjo@sirclo.com>
2023-05-03 15:03:32 +0700

    another update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,15 @@ module.exports = {
     './templates/*.liquid'
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '94': '22rem'
+      },
+      gridTemplateColumns: {
+        '4': 'repeat(4, minmax(0, 1fr))',
+        '5': 'repeat(5, minmax(0, 1fr))'
+      }
+    },
   },
   plugins: [],
 }
